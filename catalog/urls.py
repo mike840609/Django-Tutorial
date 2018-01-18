79e21f01703e1ec33dcf17109dd552a3aafb6e18
MIKE-PC\MIKE <mike840609@gmail.com>
2018-01-18 14:54:45 +0800

    5. Creating our home page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
-
+    # html can call <a href="{% url 'index' %}"> directly . 
+    path('',views.index , name = 'index') , 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Book , BookInstance , Author , Genre
 
-# Create your views here.
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact = 'a').count()
+    num_authors = Author.objects.count()
+
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
+    return render(request ,'index.html', context = {'num_books': num_books , 'num_instances': num_instances , 'num_instances_available':num_instances_available , 'num_authors':num_authors ,'num_visits':num_visits} )
